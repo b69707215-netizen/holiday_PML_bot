c681cc8f19767f73349f8d23c75a3d3eb8ac5cb2
b69707215-netizen <b69707215@gmail.com>
2026-06-10 07:53:04 +0300

    Fix missing router imports in handlers/__init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,7 @@
 from handlers.common import router as common_router
 from handlers.teacher import router as teacher_router
 from handlers.secretary import router as secretary_router
+from handlers.crm import router as crm_router
+from handlers.document_upload import router as document_upload_router
 
-__all__ = ["common_router", "teacher_router", "secretary_router"]
+__all__ = ["common_router", "teacher_router", "secretary_router", "crm_router", "document_upload_router"]
